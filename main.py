8ceebe9b1c6474d364766007615368022841c7fe
njmarko <njmarko1991@gmail.com>
2019-12-29 12:22:04 +0100

    Wrote the basic structure of the class Population

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-
+from src.model.individual import Individual
 
 
 def main():
+    ind = Individual(2, -10, 10)
+    print(ind)
     print("Hello World!")
+
 
 if __name__ == '__main__':
     main()
