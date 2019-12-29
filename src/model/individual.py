95a4c8f175a84f0bd7425592475bb97fbdec3532
syose <Pohovan1hitg>
2019-12-29 14:57:53 +0100

    Added mutation method

--- src/model/individual.py
+++ src/model/individual.py
@@ -1,5 +1,7 @@
-from random import random
+from random import random, randint, gauss
+
 from src.functions.functions import ackley, griewank, michalewicz
+
 
 class Individual(object):
 
@@ -27,6 +29,9 @@ class Individual(object):
     def get_num_of_genes(self):
         return len(self._genes)
 
+    def gene_length(self):
+        return len(self._genes)
+
     def calc_fitness(self, fnc):
         self._fitness = fnc(self._genes)
         return self._fitness
@@ -40,7 +45,32 @@ class Individual(object):
 
     def calc_fitness_michalewicz(self):
         return self.calc_fitness(michalewicz)
+
     # -----------
+
+    # lower_bond je devijacija u gausu
+    def mutation(self, mutation_rate=2, method="gauss", lower_bond=5, upper_bond=6):
+        gene_len = self.gene_length()
+        if mutation_rate > gene_len:
+            print("ee")
+            mutation_rate = gene_len
+        rand_id = []
+        while len(rand_id) < mutation_rate:
+            rand = randint(0, gene_len - 1)
+            if rand not in rand_id:
+                rand_id.append(rand)
+
+        if method == "gauss":
+            old_genes = self.get_genes()
+            for i in rand_id:
+                self._genes[i] = \
+                    self._genes[i] + gauss(0, lower_bond)
+
+        elif method == "random":
+            for i in rand_id:
+                self._genes[i] = random() * (upper_bond - lower_bond) + lower_bond
+
+        return self._genes
 
     def __str__(self):
         return str(self._genes)
