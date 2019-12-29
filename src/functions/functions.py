d4d4b52d767be3c68165d60728b1b1e6a21c0a04
syose <Pohovan1hitg>
2019-12-29 12:06:23 +0100

    Implemented griewank's function

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -1,14 +1,26 @@
 from math import sqrt,exp,cos,pi
 
 
-def ackley(axes):
-    dim = len(axes)
+def ackley(axis):
+    dim = len(axis)
     sum1 = 0;
     sum2 = 0;
 
-    for x in axes:
+    for x in axis:
         sum1 += x*x
         sum2 += cos(2*pi*x)
 
     ans = -20 * exp(-0.2 * sqrt((1/dim)*sum1)) - exp((1/dim) * sum2) + 20 + exp(1)
+    return ans
+
+
+def griewank(axis):
+    sum1 = 0
+    product1 = 1
+
+    for i,x in enumerate(axis):
+        sum1 += (x*x)/4000
+        product1 *= cos(x/sqrt(i))
+
+    ans = sum1 - product1 + 1
     return ans
