d2a4dd81a2fefaaf8d2a9daf66e8924b779ab8b3
syose <Pohovan1hitg>
2019-12-29 15:47:15 +0100

    Implemented crossover method (One, Two Point and Random crossover)

--- src/model/individual.py
+++ src/model/individual.py
@@ -20,6 +20,9 @@ class Individual(object):
     def get_genes(self):
         return self._genes
 
+    def set_genes(self, genes):
+        self._genes = genes
+
     def get_lower_bond(self):
         return self._lower_bound
 
@@ -61,7 +64,6 @@ class Individual(object):
                 rand_id.append(rand)
 
         if method == "gauss":
-            old_genes = self.get_genes()
             for i in rand_id:
                 self._genes[i] = \
                     self._genes[i] + gauss(0, lower_bond)
@@ -71,6 +73,46 @@ class Individual(object):
                 self._genes[i] = random() * (upper_bond - lower_bond) + lower_bond
 
         return self._genes
+
+    def crossover(self, other, method="One Point"):
+        size = self.gene_length()
+        ind1 = Individual(size, 0, 0)
+        ind2 = Individual(size, 0, 0)
+        genes1 = []
+        genes2 = []
+
+        if method == "two point":
+            if size == 1:
+                method = "one point"
+            else:
+                point1 = randint(0, size)
+                point2 = randint(0, size)
+                while point1 == point2:
+                    point2 = randint(0, size)
+
+                genes1 = self.get_genes()[0:point1] + other.get_genes()[point1:point2] + self.get_genes()[point2:]
+                genes2 = other.get_genes()[0:point1] + self.get_genes()[point1:point2] + other.get_genes()[point2:]
+
+        if method == "one point":
+            point1 = randint(0, size - 1)
+
+            genes1 = self.get_genes()[0:point1] + other.get_genes()[point1:]
+            genes2 = other.get_genes()[0:point1] + self.get_genes()[point1:]
+            print("more")
+
+        elif method == "random":
+            for i, j in zip(self.get_genes(), other.get_genes()):
+                if random() > 0.5:
+                    genes1.append(i)
+                    genes2.append(j)
+                else:
+                    genes1.append(j)
+                    genes2.append(i)
+
+        ind1.set_genes(genes1)
+        ind2.set_genes(genes2)
+
+        return [ind1, ind2]
 
     def __str__(self):
         return str(self._genes)
