58620a3fb14c5216a0fe3afb6322ccd3ee005959
njmarko <njmarko1991@gmail.com>
2020-01-08 21:41:16 +0100

    Connected the run button so it runs the ga algorithm now
    Removed import from QT5 for combo box. Combo box is now imported from pyside2
    
    Tweaked the default parameters

--- src/gui/center/inputs_layout.py
+++ src/gui/center/inputs_layout.py
@@ -1,11 +1,11 @@
-from PyQt5.QtWidgets import QComboBox
+# from PyQt5.QtWidgets import QComboBox
 from PySide2.QtCore import Qt
 from PySide2.QtGui import QFont
 from PySide2.QtWidgets import QFormLayout, QHBoxLayout, QPushButton, QDoubleSpinBox, QComboBox, QRadioButton, QLabel, QSpinBox
-
 from src.functions.functions import ackley, michalewicz, griewank
 from src.gui.Separator import QHLine
-
+from src.ga import ga
+from src.functions.functions import ackley,griewank,michalewicz
 
 # class QHLine(QFrame):
 #     def __init__(self):
@@ -64,6 +64,10 @@ class InputsLayout(QFormLayout):
         self.init_row_mutation()
         self.init_row_run()
 
+        # connect buttons
+        self.btn_run.clicked.connect(self.run_button_clicked)
+
+
     def init_fonts(self):
         self.big_font.setPointSizeF(14)
         self.medium_font.setPointSizeF(12)
@@ -120,11 +124,11 @@ class InputsLayout(QFormLayout):
         self.inp_average_result.setMinimum(-100000)
         self.inp_average_result.setMaximum(100000)
 
-        self.inp_max_iter.setValue(10)
+        self.inp_max_iter.setValue(100)
         self.inp_similarity.setValue(60)
         # TODO: Checkbox for NONE value
         self.inp_best_result.setValue(-10)
-        self.inp_average_result.setValue(10)
+        self.inp_average_result.setValue(0)
 
         self.addRow(self.header_stop)
         self.addRow("Max iter", self.inp_max_iter)
@@ -142,7 +146,7 @@ class InputsLayout(QFormLayout):
         self.inp_selection_method.addItem("Random", "Random")
         self.inp_selection_method.addItem("No Selection", "No Selection")
         self.inp_elitism.setMaximum(1)
-        self.inp_elitism.setValue(0.2)
+        self.inp_elitism.setValue(0.05)
 
         self.addRow(self.header_selection)
         self.addRow("Selection Method", self.inp_selection_method)
@@ -164,15 +168,19 @@ class InputsLayout(QFormLayout):
         self.header_crossover.setFont(self.medium_font)
         self.header_crossover.setText("Crossover")
 
-        self.inp_crossover_method.addItem("One point", "One point")
-        self.inp_crossover_method.addItem("Two point", "Two point")
         self.inp_crossover_method.addItem("Intermediate", "Intermediate")
         self.inp_crossover_method.addItem("Line Intermediate", "Line Intermediate")
+        self.inp_crossover_method.addItem("One point", "One point")
+        self.inp_crossover_method.addItem("Two point", "Two point")
+
         # TODO: Heuristic
         self.inp_crossover_method.addItem("Heuristic (TO DO)", "Heuristic")
         self.inp_crossover_method.addItem("Random", "Random")
+        self.inp_mutation_method.setCurrentIndex(2)
         self.inp_crossover_fraction.setMaximum(1)
+        self.inp_crossover_fraction.setValue(0.8)
         self.intermediate_offset.setMaximum(20)
+        self.intermediate_offset.setValue(1.2)
 
         self.addRow(self.header_crossover)
         self.addRow("Crossover Method", self.inp_crossover_method)
@@ -200,3 +208,74 @@ class InputsLayout(QFormLayout):
 
         self.addRow(self.header_run)
         self.addRow(self.btn_run)
+
+
+    def run_button_clicked(self):
+
+        function = self.inp_functions_combo.currentData()
+
+        if self.inp_extrema_min.isChecked():
+            extrem = 0
+        else:
+            extrem = 1
+        pop_size = self.inp_pop_size.text()
+        low_bound = self.inp_lower_bound.text()
+        upp_bound = self.inp_upper_bound.text()
+        max_iter = self.inp_max_iter.text()
+        sim_results = self.inp_similarity.text()
+        best_res = self.inp_best_result.text()
+        average_res = self.inp_average_result.text()
+        select_method = self.inp_selection_method.currentText()
+        elit_percent = self.inp_elitism.text()
+        pairing = self.inp_pairing_method.currentText()
+        crossover_method = self.inp_crossover_method.currentText()
+        crossover_fraction = self.inp_crossover_fraction.text()
+        intermediate_offset = self.intermediate_offset.text()
+        mutation_method = self.inp_mutation_method.currentText()
+        mutation_intensity = self.inp_mutation_intensity.text()
+
+        #za debug
+        print(function)
+        print(extrem)
+        print(pop_size)
+        print(low_bound)
+        print(upp_bound)
+        print(max_iter)
+        print(sim_results)
+        print(best_res)
+        print(average_res)
+        print(select_method)
+        print(elit_percent)
+        print(pairing)
+        print(crossover_method)
+        print(crossover_fraction)
+        print(intermediate_offset)
+        print(mutation_method)
+        print(mutation_intensity)
+
+
+        options = {
+            "pop_size": int(pop_size),
+            "max_iter": int(max_iter),
+            "lower_bound": float(low_bound),
+            "upper_bound": float(upp_bound),
+            "find_max": extrem,
+            "prints": 1,
+            "average_result": float(average_res),
+            "best_result": float(best_res),
+            "similarity": float(sim_results),
+            "selection": select_method,
+            "pairing": pairing,
+            "crossover": crossover_method,
+            "crossover_fraction": float(crossover_fraction),
+            "intermediate_offset": float(intermediate_offset),
+            # 0 mean child will be between parents, 1 mean offset is same as two parent distance
+            "mutation": mutation_method,
+            "mutate_fraction": float(mutation_intensity),
+            "elitism": float(elit_percent)
+        }
+
+        res = ga(function, 2, options)
+
+
+
